d9ac4a30c80035b5c4c88b7b533c4e29c15e4541
Sevdalin Zhelyazkov <irrealizable@abv.bg>
2016-06-18 18:59:55 +0300

    Test Commit

---
+++ src/main/webapp/js/main.js
@@ -0,0 +1,98 @@
+$(document).ready(function() {
+    var $LoginButton = $("#login-button"),
+        $RegisterButton = $("#register-button");
+    $LoginButton.on("click", function() {
+        callForLogin();
+    });
+
+    $('#login-password').keydown(function(e) {
+        if (e.keyCode == 13) {
+            callForLogin();
+        }
+    });
+
+    $RegisterButton.click(function() {
+        var oRegisterData = {
+            user: {
+                firstName: $("#register-firstName").val(),
+                lastName: $("#register-lastName").val(),
+                userName: $("#register-userName").val(),
+                password: $("#register-password").val()
+            }
+        };
+
+        if (!validate()) {
+            return;
+        }
+
+        $.ajax({
+                url: 'rest/user/register',
+                type: "POST",
+                contentType: "application/json",
+                data: JSON.stringify(oRegisterData)
+            })
+            .success(function(data) {
+                alert("Register success! You can now login in the system!");
+            })
+            .fail(function(data) {
+                alert("Invalid data or user with this data already exists!");
+            })
+            .always(function() {
+                $("#register-form").submit();
+            });
+    });
+
+    function callForLogin() {
+
+        var oUserData = {
+            user: {
+                userName: $("#login-userName").val(),
+                password: $("#login-password").val()
+            }
+        };
+
+        console.log(JSON.stringify(oUserData));
+
+
+        $.ajax({
+            url: 'rest/user/login',
+            type: "POST",
+            contentType: "application/json",
+            data: JSON.stringify(oUserData),
+            success: function(response) {
+                console.log("FDFGGDVF");
+            },
+            statusCode: {
+                401: function() {
+                    console.log("Wrong username or password!");
+                },
+                200: function() {
+                	console.log("Success 200 login!");
+                }
+            }
+        });
+    }
+
+    function DO_NOT_EVER_WRITE_SUCH_CODE(string, position) {
+        return string.responseText.split(',')[position].split(':')[1].slice(1, (string.responseText.split(',')[position].split(':')[1].length));
+    }
+
+    function validate() {
+        var $FirstName = $('#register-firstName'),
+            $LastName = $('#register-lastName'),
+            $Email = $('#register-email'),
+            $ReEmail = $('#register-confirm-email'),
+            $Password = $('#register-password');
+
+        // function validateEmail(email) {
+        //     var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+        //     return re.test(email);
+        // }
+
+        // if (($FirstName.val().length === 0) || ($FirstName.val().length > 20) || ($LastName.val().length === 0) || ($LastName.val().length > 20) || (!validateEmail($Email.val())) || $ReEmail.val() !== $Email.val() || $Password.val().length < 8) {
+        //     alert('Incorrect data! Check again!');
+        //     return false;
+        // }
+        return true;
+    }
+});
